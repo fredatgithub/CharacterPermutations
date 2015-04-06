87c75b3ff4ccf5fb2cd5421a1db99fd6dc23a30c
Freddy Juhel <domaine75@gmail.com>
2015-04-06 15:16:58 +0200

    Initial commit

---
+++ PermutationCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PermutationCalculator
+{
+  class Program
+  {
+    static void Main()
+    {
+      string inputString = Console.ReadLine();
+      PermutationCalculator permutationCalculator = new PermutationCalculator();
+      var result = permutationCalculator.Calculate(inputString.ToCharArray());
+      Console.WriteLine("Total items:" + result.Count);
+      foreach (var item in result)
+      {
+        Console.WriteLine(item);
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
